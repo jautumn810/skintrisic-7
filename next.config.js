04fcb721e8c71a40f9515835ff5b6c4e7a1ede71
Autumn <jautumn810@gmail.com>
2025-12-27 17:46:46 -0600

    Clean up build artifacts and update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 }
 
 export default nextConfig
